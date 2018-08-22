f1e3bb2ae84ad7149613d94513daa6677ea9e0fb
celestino.salim@gmail.com <celestino.salim@gmail.com>
2018-08-22 11:31:59 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,7 +3,7 @@ def using_push(array, updated_array)
   array.push(updated_array)
 end
 
-def using_push(array, updated_array)
+def using_unshift(array, updated_array)
   array = []
-  array.shift(updated_array)
+  array.unshift(updated_array)
 end
