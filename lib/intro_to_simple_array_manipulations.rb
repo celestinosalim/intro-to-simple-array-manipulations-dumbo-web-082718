b73a4afc9b430ef1b718417066c1bc7daf2ac82b
celestino.salim@gmail.com <celestino.salim@gmail.com>
2018-08-22 11:37:51 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -5,8 +5,8 @@ end
 def using_unshift(array, updated_array)
   array.unshift(updated_array)
 end
-def using_pop(array, updated_array)
-  array.pop(updated_array)
+def using_pop(array)
+  array.pop()
 end
 def pop_with_args(array)
   array.pop(2)
